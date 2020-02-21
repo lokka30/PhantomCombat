51d13837092f488bf1acb940acd442776ee52166
lokka30 <lokka30@protonmail.com>
2020-02-21 22:21:12 +0800

    1.11-SNAPSHOT Build 1

--- src/main/java/io/github/lokka30/phantomcombat/listeners/LPvPToggle.java
+++ src/main/java/io/github/lokka30/phantomcombat/listeners/LPvPToggle.java
@@ -1,10 +1,11 @@
 package io.github.lokka30.phantomcombat.listeners;
 
 import io.github.lokka30.phantomcombat.PhantomCombat;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PotionSplashEvent;
 
 public class LPvPToggle implements Listener {
 
@@ -13,20 +14,50 @@ public class LPvPToggle implements Listener {
     @EventHandler
     public void onEntityDamageByEntity(final EntityDamageByEntityEvent e) {
         if (instance.settings.get("pvp-toggle.enable", true)) {
-            if (e.getDamager() instanceof Player && e.getEntity() instanceof Player) {
-                final Player defender = (Player) e.getEntity();
-                final Player attacker = (Player) e.getDamager();
-
-                if (!instance.data.get("players." + defender.getUniqueId().toString() + ".pvp", true)) {
-                    attacker.sendMessage(instance.colorize(instance.messages.get("pvp-toggle.protected-target", "%target% has PvP disabled").replaceAll("%target%", defender.getDisplayName())));
-                    e.setCancelled(true);
-                }
-
-                if (!instance.data.get("players." + attacker.getUniqueId().toString() + ".pvp", true)) {
-                    attacker.sendMessage(instance.colorize(instance.messages.get("pvp-toggle.protected-you", "You have PvP disabled")));
+            if (e.getEntity() instanceof Player) {
+                if (checkPvPToggle((Player) e.getEntity(), e.getDamager())) {
                     e.setCancelled(true);
                 }
             }
         }
+    }
+
+    @EventHandler
+    public void onSplash(final PotionSplashEvent e) {
+        for (LivingEntity livingEntity : e.getAffectedEntities()) {
+            if (livingEntity instanceof Player) {
+                final Player p = (Player) livingEntity;
+                if (!(instance.data.get("players." + p.getUniqueId().toString() + ".pvp", true))) {
+                    e.getAffectedEntities().remove(livingEntity);
+                }
+            }
+        }
+    }
+
+    private boolean checkPvPToggle(Player defender, Entity attackerEntity) {
+        final boolean defenderPvP = instance.data.get("players." + defender.getUniqueId().toString() + ".pvp", true);
+
+        if (attackerEntity instanceof Player) {
+            final Player attacker = (Player) attackerEntity;
+
+            if (!defenderPvP) {
+                attacker.sendMessage(instance.colorize(instance.messages.get("pvp-toggle.protected-target", "%target% has PvP disabled").replaceAll("%target%", defender.getDisplayName())));
+                return true;
+            }
+
+            if (!instance.data.get("players." + attacker.getUniqueId().toString() + ".pvp", true)) {
+                attacker.sendMessage(instance.colorize(instance.messages.get("pvp-toggle.protected-you", "You have PvP disabled")));
+                return true;
+            }
+        } else if (attackerEntity instanceof Arrow) {
+            return ((Arrow) attackerEntity).getShooter() instanceof Player && !defenderPvP;
+        } else if (attackerEntity instanceof Egg) {
+            return ((Egg) attackerEntity).getShooter() instanceof Player && !defenderPvP;
+        } else if (attackerEntity instanceof Snowball) {
+            return ((Snowball) attackerEntity).getShooter() instanceof Player && !defenderPvP;
+        } else if (attackerEntity instanceof Trident) {
+            return ((Trident) attackerEntity).getShooter() instanceof Player && !defenderPvP;
+        }
+        return false;
     }
 }
---
+++ src/main/java/io/github/lokka30/phantomcombat/utils/Utils.java
@@ -0,0 +1,20 @@
+package io.github.lokka30.phantomcombat.utils;
+
+public class Utils {
+
+    public static String getRecommendedServerVersion() {
+        return "1.15";
+    }
+
+    public static int getRecommendedSettingsVersion() {
+        return 7;
+    }
+
+    public static int getRecommendedMessagesVersion() {
+        return 4;
+    }
+
+    public static int getRecommendedDataVersion() {
+        return 1;
+    }
+}
